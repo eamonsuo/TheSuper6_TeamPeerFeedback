6de4c1c5b9fb40b3a0b9e5786702d854c6befab8
JackJobling <jjobling202@gmail.com>
2022-09-14 15:03:05 +1000

    Added embedded mode for ProgressSlider

--- lib/widgets/progress_slider.dart
+++ lib/widgets/progress_slider.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 class ProgressSlider extends StatefulWidget {
 
   double? currentProgress;
-  ProgressSlider({this.currentProgress});
+  bool embeded;
+  ProgressSlider({this.currentProgress, this.embeded = false});
 
   @override
   State<ProgressSlider> createState() => _ProgressSlider();
@@ -22,20 +23,40 @@ class _ProgressSlider extends State<ProgressSlider> {
     progress = widget.currentProgress ?? 0;
   }
 
+
+
   @override
   Widget build(BuildContext context) {
+    final embededStyle = SliderTheme.of(context).copyWith(
+      trackHeight: 18,
+      thumbColor: Colors.transparent,
+      overlayColor: Colors.transparent,
+      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 0.0)
+    );
+    final defaultStyle = SliderTheme.of(context).copyWith(
+      trackHeight: 18,
+      thumbColor: UIColours.background,
+      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 14.0)
+    );
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Progesss: ${progress * 100 ~/ 1}%", style: TextStyle(color: UIColours.darkBlue),),
-        Slider(
-          value: progress, 
-          onChanged: (double value) {
-            setState(() {
-              progress = value;
-            });
-          }
-        ),
+        Text("Progesss: ${progress.floor()}%", style: TextStyle(color: UIColours.darkBlue),),
+        SliderTheme(
+          data: widget.embeded? embededStyle: defaultStyle, 
+          child: Slider(
+            min: 0,
+            max: 100,
+            value: progress, 
+            onChanged: (double value) {
+              if (!widget.embeded){
+                setState(() {
+                  progress = value;
+                });
+              }
+            }
+          ),
+        )
       ],
     );
   }
--- lib/widgets/student_task.dart
+++ lib/widgets/student_task.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:deco3801_project/util/ui_colours.dart';
 import 'package:deco3801_project/widgets/progress_slider.dart';
 import 'package:deco3801_project/widgets/student_task_buttons.dart';
@@ -9,9 +11,11 @@ class StudentTask extends StatelessWidget {
   ///    Truncate long text and have a '...read mode'
   ///    
 
-  String teamName;
+  String taskTitle;
   String taskDescription;
-  StudentTask(this.teamName, this.taskDescription);
+  bool embeded;
+
+  StudentTask(this.taskTitle, this.taskDescription, {this.embeded = false});
 
 
   @override
@@ -29,15 +33,37 @@ class StudentTask extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Team: $teamName", style: TextStyle(color: UIColours.darkBlue)),
+                Text(taskTitle, style: TextStyle(color: UIColours.darkBlue)),
                 StudentTaskButtons()
               ],
             ),
             Text(taskDescription, style: TextStyle(color: UIColours.lightBlue)),
-            ProgressSlider()
+            // embeded? ProgesssBar(0.69): ProgressSlider()
+            ProgressSlider(embeded: embeded,)
           ],
         ),
       )
+    );
+  }
+}
+
+class ProgesssBar extends StatelessWidget {
+
+  double progress;
+  ProgesssBar(this.progress);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Progesss: ${progress * 100 ~/ 1}%", style: TextStyle(color: UIColours.darkBlue),),
+        LinearProgressIndicator(
+          value: progress,
+          minHeight: 10,
+        ),
+      ],
     );
   }
 }
