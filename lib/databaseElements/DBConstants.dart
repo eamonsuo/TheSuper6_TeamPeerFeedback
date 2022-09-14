acf966bfab120f9e12def8265eeb50c1de312c1b
Hayden <h.padget@uq.net.au>
2022-09-14 14:53:45 +1000

    Remove HEAD thingo

--- lib/databaseElements/DBConstants.dart
+++ lib/databaseElements/DBConstants.dart
@@ -1,10 +1,7 @@
 class DBConstants {
   static const url = 'https://vegetarian-twenties.000webhostapp.com/query.php';
   // static const url = 'https://deco3801-thesupersix.uqcloud.net/getData.php';
-<<<<<<< HEAD
 
-=======
->>>>>>> e404603d0d4287b946ce84ddad1970553c8b5f3c
   static const String GET_ALL_ACTION = 'GET_ALL';
   static const String GET_ONE_ACTION = 'GET_SELECTION';
   static const String ADD_ACTION = 'ADD_RECORD';
