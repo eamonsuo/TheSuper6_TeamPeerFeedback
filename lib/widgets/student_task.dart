70d51391fff4501dfed757828f61cb4071f1ea4b
JackJobling <jjobling202@gmail.com>
2022-09-14 13:59:24 +1000

    Refactored StudentTask class to the builtin Card class.

--- lib/widgets/student_task.dart
+++ lib/widgets/student_task.dart
@@ -13,36 +13,31 @@ class StudentTask extends StatelessWidget {
   String taskDescription;
   StudentTask(this.teamName, this.taskDescription);
 
-  // This spaghetti is brought to you by Jack, I intend to de-spaghetti it at some point.
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.95,
-        decoration: BoxDecoration(
-          color: UIColours.white,
-          borderRadius: BorderRadius.all(Radius.circular(10))
+    return Card(
+      color: UIColours.white,
+      margin: EdgeInsets.all(10),
+      
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Team: $teamName", style: TextStyle(color: UIColours.darkBlue)),
+                StudentTaskButtons()
+              ],
+            ),
+            Text(taskDescription, style: TextStyle(color: UIColours.lightBlue)),
+            ProgressSlider()
+          ],
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Team: $teamName", style: TextStyle(color: UIColours.darkBlue)),
-                  StudentTaskButtons()
-                ],
-              ),
-              Text(taskDescription, style: TextStyle(color: UIColours.lightBlue)),
-              ProgressSlider()
-            ],
-          ),
-        ),
-      ),
+      )
     );
   }
 }
