f19326f82729cc8cc72c39f44fd115e16d786e6f
micah123455 <6lstubbsy@gmail.com>
2022-10-16 12:50:08 +1000

    Added reply functionality to the messages

--- lib/pages/tutor_activity.dart
+++ lib/pages/tutor_activity.dart
@@ -16,6 +16,9 @@ class _TutorActivityPageState extends State<TutorActivityPage> {
   late Future<List<Map<String, String>>> _teams;
   late Future<List<Map<String, String>>> _users;
 
+  TextEditingController tutorResponseMessageController =
+      TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -55,7 +58,7 @@ class _TutorActivityPageState extends State<TutorActivityPage> {
             }*/
 
             return Scaffold(
-                appBar: AppBar(title: const Text('Student Activity')),
+                appBar: AppBar(title: const Text('Tutor Activity')),
                 body: Container(
                     padding: const EdgeInsets.all(10),
                     child: SingleChildScrollView(
@@ -79,6 +82,152 @@ class _TutorActivityPageState extends State<TutorActivityPage> {
                             subtitle: Text(
                                 //"Message from ${_recievedMessagesData.elementAt(index)['sender_id']}"),
                                 "Message from ${_userData.firstWhere((element) => element["user_id"] == _recievedMessagesData.elementAt(index)['sender_id'])["username"]} regarding ${_teamsData.firstWhere((element) => element["team_id"] == _recievedMessagesData.elementAt(index)['team_id'])["team_name"]}"), //Should include tutor name and team name
+                            trailing: TextButton(
+                              child: const Text("Reply"),
+                              onPressed: () {
+                                tutorResponseMessageController.clear();
+                                showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                          insetPadding: EdgeInsets.only(
+                                              left: 20, right: 20),
+                                          scrollable: true,
+                                          title: Row(
+                                            children: [
+                                              const Text(
+                                                "Reply",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 17,
+                                                    color: Color.fromRGBO(
+                                                        21, 90, 148, 10)),
+                                              ),
+                                              const Spacer(),
+                                              IconButton(
+                                                onPressed: () =>
+                                                    Navigator.of(context).pop(),
+                                                icon: const Icon(Icons.close),
+                                                splashRadius: 15,
+                                              )
+                                            ],
+                                          ),
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                              side: const BorderSide(
+                                                  color: Color.fromRGBO(
+                                                      21, 90, 148, 10),
+                                                  width: 1.5)),
+                                          content: Column(
+                                            children: [
+                                              Container(
+                                                  width: MediaQuery.of(context)
+                                                          .size
+                                                          .width *
+                                                      0.7,
+                                                  height: MediaQuery.of(context)
+                                                          .size
+                                                          .height *
+                                                      0.7,
+                                                  child: TextField(
+                                                    controller:
+                                                        tutorResponseMessageController,
+                                                    maxLines: null,
+                                                    textAlign: TextAlign.left,
+                                                    decoration: InputDecoration(
+                                                        border:
+                                                            InputBorder.none,
+                                                        enabledBorder:
+                                                            OutlineInputBorder(
+                                                          borderSide:
+                                                              const BorderSide(
+                                                                  width: 3,
+                                                                  color: Colors
+                                                                      .blue),
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(15),
+                                                        ),
+                                                        focusedBorder:
+                                                            OutlineInputBorder(
+                                                          borderSide:
+                                                              const BorderSide(
+                                                                  width: 3,
+                                                                  color: Colors
+                                                                      .red),
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(15),
+                                                        )),
+                                                  )),
+                                              TextButton(
+                                                  onPressed: () async {
+                                                    if (_recievedMessagesData
+                                                            .elementAt(index)
+                                                            .entries
+                                                            .elementAt(4)
+                                                            .value !=
+                                                        "NULL") {
+                                                      await TutorMessagesTable.addMessage(
+                                                          tutorId,
+                                                          _recievedMessagesData
+                                                              .elementAt(index)
+                                                              .entries
+                                                              .elementAt(1)
+                                                              .value,
+                                                          _recievedMessagesData
+                                                              .elementAt(index)
+                                                              .entries
+                                                              .elementAt(3)
+                                                              .value,
+                                                          tutorId,
+                                                          tutorResponseMessageController
+                                                              .text,
+                                                          subGoalId:
+                                                              _recievedMessagesData
+                                                                  .elementAt(
+                                                                      index)
+                                                                  .entries
+                                                                  .elementAt(4)
+                                                                  .value);
+                                                    } else {
+                                                      await TutorMessagesTable
+                                                          .addMessage(
+                                                        tutorId,
+                                                        _recievedMessagesData
+                                                            .elementAt(index)
+                                                            .entries
+                                                            .elementAt(1)
+                                                            .value,
+                                                        _recievedMessagesData
+                                                            .elementAt(index)
+                                                            .entries
+                                                            .elementAt(3)
+                                                            .value,
+                                                        tutorId,
+                                                        tutorResponseMessageController
+                                                            .text,
+                                                        /*subGoalId:
+                                                            _recievedMessagesData
+                                                                .elementAt(
+                                                                    index)
+                                                                .entries
+                                                                .elementAt(4)
+                                                                .value*/
+                                                      );
+                                                    }
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: const Text("Send"))
+                                            ],
+                                          )
+                                          /**/
+                                          );
+                                    });
+                                //TutorMessagesTable.addMessage(tutorId, _recievedMessagesData.elementAt(index).entries.elementAt(1).value, _recievedMessagesData.elementAt(index).entries.elementAt(3).value, tutorId, messageString)
+                              },
+                            ),
                           );
                         },
                         separatorBuilder: (BuildContext context, int index) =>
